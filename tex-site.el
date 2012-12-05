fcdf3cb1c14127b1d961d2b68f4fb264cdc095d4
Davide G. M. Salvetti <salve@debian.org>
2012-12-06 00:04:00 +0100

    Imported Upstream version 9.9b

--- tex-site.el
+++ tex-site.el
@@ -5,7 +5,7 @@
 
 ;; Author: Per Abrahamsen <abraham@dina.kvl.dk>
 ;; Maintainer: Per Abrahamsen <auc-tex@sunsite.auc.dk>
-;; Version: 9.9a
+;; Version: 9.9b
 ;; Keywords: wp
 
 ;; This program is free software; you can redistribute it and/or modify
